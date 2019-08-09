14932aa8286db3ddb27b16b16476636b453ccea6
KK998 <klemen.komel@gmail.com>
2019-08-09 19:13:07 +0200

    Final version of game

---
+++ Ball.lua
@@ -0,0 +1,71 @@
+--[[
+    GD50 (harward online course) 2019
+    Pong Remake
+
+    -- Main Program --
+
+    Author: KK998
+
+    Originally programmed by Atari in 1972. Features two
+    paddles, controlled by players, with the goal of getting
+    the ball past your opponent's edge. First to 10 points wins.
+
+    This version is built to more closely resemble the NES than
+    the original Pong machines or the Atari 2600 in terms of
+    resolution, though in widescreen (16:9) so it looks nicer on 
+    modern systems.
+]]
+
+Ball = Class{}
+
+function Ball:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+
+    -- these variables are for keeping track of our velocity on both the
+    -- X and Y axis, since the ball can move in two dimensions
+    self.dy = 0
+    self.dx = 0
+end
+
+--[[
+    Expects a paddle as an argument and returns true or false, depending
+    on whether their rectangles overlap.
+]]
+function Ball:collides(paddle)
+    -- first, check to see if the left edge of either is farther to the right
+    -- than the right edge of the other
+    if self.x > paddle.x + paddle.width or paddle.x > self.x + self.width then
+        return false
+    end
+
+    -- then check to see if the bottom edge of either is higher than the top
+    -- edge of the other
+    if self.y > paddle.y + paddle.height or paddle.y > self.y + self.height then
+        return false
+    end 
+
+    -- if the above aren't true, they're overlapping
+    return true
+end
+
+--[[
+    Places the ball in the middle of the screen, with no movement.
+]]
+function Ball:reset()
+    self.x = VIRTUAL_WIDTH / 2 - 2
+    self.y = VIRTUAL_HEIGHT / 2 - 2
+    self.dx = 0
+    self.dy = 0
+end
+
+function Ball:update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+end
+
+function Ball:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
---
+++ Paddle.lua
@@ -0,0 +1,67 @@
+--[[
+    GD50 (harward online course) 2019
+    Pong Remake
+
+    -- Main Program --
+
+    Author: KK998
+
+    Originally programmed by Atari in 1972. Features two
+    paddles, controlled by players, with the goal of getting
+    the ball past your opponent's edge. First to 10 points wins.
+
+    This version is built to more closely resemble the NES than
+    the original Pong machines or the Atari 2600 in terms of
+    resolution, though in widescreen (16:9) so it looks nicer on 
+    modern systems.
+]]
+
+Paddle = Class{}
+
+--[[
+    The `init` function on our class is called just once, when the object
+    is first created. Used to set up all variables in the class and get it
+    ready for use.
+
+    Our Paddle should take an X and a Y, for positioning, as well as a width
+    and height for its dimensions.
+
+    Note that `self` is a reference to *this* object, whichever object is
+    instantiated at the time this function is called. Different objects can
+    have their own x, y, width, and height values, thus serving as containers
+    for data. In this sense, they're very similar to structs in C.
+]]
+function Paddle:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.dy = 0
+end
+
+function Paddle:update(dt)
+    -- math.max here ensures that we're the greater of 0 or the player's
+    -- current calculated Y position when pressing up so that we don't
+    -- go into the negatives; the movement calculation is simply our
+    -- previously-defined paddle speed scaled by dt
+    if self.dy < 0 then
+        self.y = math.max(0, self.y + self.dy * dt)
+    -- similar to before, this time we use math.min to ensure we don't
+    -- go any farther than the bottom of the screen minus the paddle's
+    -- height (or else it will go partially below, since position is
+    -- based on its top left corner)
+    else
+        self.y = math.min(VIRTUAL_HEIGHT - self.height, self.y + self.dy * dt)
+    end
+end
+
+--[[
+    To be called by our main function in `love.draw`, ideally. Uses
+    LÖVE2D's `rectangle` function, which takes in a draw mode as the first
+    argument as well as the position and dimensions for the rectangle. To
+    change the color, one must call `love.graphics.setColor`. As of the
+    newest version of LÖVE2D, you can even draw rounded rectangles!
+]]
+function Paddle:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
